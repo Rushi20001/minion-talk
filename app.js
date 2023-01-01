3777a5607fbbcebd29d670d0e0f3bb3b5ba0c976
Rushi20001 <rajwaderishi17@gmail.com>
2023-01-01 20:49:30 +0530

    input output processing

---
+++ app.js
@@ -0,0 +1,14 @@
+var btnTranslate= document.querySelector("#btn-translate")
+var txtInput= document.querySelector("#txt-input")
+var outputDiv=document.querySelector("#output")
+
+//outputDiv.innerText="hello"
+
+console.log(outputDiv)
+function clickHandler() {
+     console.log("clicked")
+    }
+
+    btnTranslate.addEventListener("click", clickHandler)
+
+
